a278fe4dbd98c1ae8410eb54968f3e95fff7363c
Paula Kuwałek <paula.kuwalek@gmail.com>
2019-11-25 19:27:26 +0100

    create a base of app plus webpack config

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+module.exports = {
+    entry: path.resolve(__dirname, 'src/index.js'),
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'style.css',
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.scss$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader'
+                ],
+            },
+        ],
+    }
+};
