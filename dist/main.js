dfaae994f29eab3443fb34f9c18eca7a20d3749b
Paula Kuwałek <paula.kuwalek@gmail.com>
2020-04-02 15:15:53 +0200

    add nav items functionality, searchbar and styling

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,124 @@
-!function(n){var e={};function t(a){if(e[a])return e[a].exports;var r=e[a]={i:a,l:!1,exports:{}};return n[a].call(r.exports,r,r.exports,t),r.l=!0,r.exports}t.m=n,t.c=e,t.d=function(n,e,a){t.o(n,e)||Object.defineProperty(n,e,{enumerable:!0,get:a})},t.r=function(n){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(n,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(n,"__esModule",{value:!0})},t.t=function(n,e){if(1&e&&(n=t(n)),8&e)return n;if(4&e&&"object"==typeof n&&n&&n.__esModule)return n;var a=Object.create(null);if(t.r(a),Object.defineProperty(a,"default",{enumerable:!0,value:n}),2&e&&"string"!=typeof n)for(var r in n)t.d(a,r,function(e){return n[e]}.bind(null,r));return a},t.n=function(n){var e=n&&n.__esModule?function(){return n.default}:function(){return n};return t.d(e,"a",e),e},t.o=function(n,e){return Object.prototype.hasOwnProperty.call(n,e)},t.p="",t(t.s=0)}([function(n,e,t){"use strict";t.r(e);t(1);const a={getArticles:()=>fetch("http://localhost").then(n=>n.json())};!function(){const n=({urlToImage:n,title:e,description:t,content:a,url:r})=>`\n        <div class="article-card">\n            <img src="${n}" class="image">\n            <h1 class="title">${e}</h1>\n            <h3 class="description">${(n=>{let e=n.match(/(.*)(Read more\.\.\.)/);return e?e[1]:n})(t)}</h3>\n            <p class="content">${(n=>{console.log(n);let e=n.match(/(.*)(\[\+\d+ chars\])$/s);return console.log(e),e?e[1]:n})(a)}</p>\n            <button type="button" class="read-more-btn" onclick="window.open('${r}')">\n                Read more\n            </button>\n        </div>\n    `;a.getArticles().catch(n=>{throw document.getElementById("main").innerHTML=`\n        <h1 style="color: red">${n}</h1>\n        <h2>${n.message}</h2>\n        `,n}).then(e=>{const t=`\n        <header>\n            \n        <nav class="navbar navbar-expand-lg navbar-light bg-light">\n            <a class="navbar-brand" href="#"><img class="logo" src="./../src/amazonlogocut.png" alt="">NEWSFEED</a>\n            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">\n                <span class="navbar-toggler-icon"></span>\n            </button>\n            <div class="collapse navbar-collapse" id="navbarSupportedContent">\n            <ul class="navbar-nav mr-auto">\n                <li class="nav-item">\n                    <a class="nav-link" href="#">Jeff Bezos</a>\n                </li>\n                <li class="nav-item">\n                    <a class="nav-link" href="#">Alexa</a>\n                </li>\n                <li class="nav-item">\n                    <a class="nav-link" href="#">Grand Tour</a>\n                </li>\n            </ul>\n            <form class="form-inline my-2 my-lg-0">\n                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">\n                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>\n            </form>\n            </div>\n        </nav>\n    \n        </header>\n        <div class="page-content">\n            <div id="articles">\n                ${e.map(e=>n(e)).join("")}\n            </div>\n            <div id="top-articles">\n                <h3>most popular articles</h3>\n            </div>\n        </div>\n        `;document.getElementById("main").innerHTML=t})}()},function(n,e,t){}]);
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./src/index.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./src/amazonlogocut.png":
+/*!*******************************!*\
+  !*** ./src/amazonlogocut.png ***!
+  \*******************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = (__webpack_require__.p + \"7972647f72c1f79cb3a5c46e21f3d817.png\");\n\n//# sourceURL=webpack:///./src/amazonlogocut.png?");
+
+/***/ }),
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _style_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./style.scss */ \"./src/style.scss\");\n/* harmony import */ var _style_scss__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_style_scss__WEBPACK_IMPORTED_MODULE_0__);\n/* harmony import */ var _src_amazonlogocut_png__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./../src/amazonlogocut.png */ \"./src/amazonlogocut.png\");\n\r\n\r\nconst apiRoute = \"http://localhost\";\r\n\r\nconst Api = {\r\n    getArticles: () => {\r\n        return fetch(apiRoute).then((response) => response.json());\r\n    }\r\n};\r\n\r\nconst QuerySearch = {\r\n    getArticles: (query) => {\r\n        return fetch(`${apiRoute}/${query}`).then((response) => response.json());\r\n    }\r\n}\r\n\r\nconst navbar = () => {\r\n    const navbarContent = `\r\n    <nav class=\"navbar navbar-expand-lg navbar-light bg-light fixed-top\">\r\n        <span id=\"logo\" class=\"logo navbar-brand\" href=\"#\"><img class=\"logo\" src=${_src_amazonlogocut_png__WEBPACK_IMPORTED_MODULE_1__[\"default\"]} alt=\"\">NEWSFEED</span>\r\n        <button class=\"navbar-toggler\" type=\"button\" data-toggle=\"collapse\" data-target=\"#navbarSupportedContent\" aria-controls=\"navbarSupportedContent\" aria-expanded=\"false\" aria-label=\"Toggle navigation\">\r\n            <span class=\"navbar-toggler-icon\"></span>\r\n        </button>\r\n        <div class=\"collapse navbar-collapse\" id=\"navbarSupportedContent\">\r\n        <ul class=\"navbar-nav mr-auto\">\r\n            <li class=\"nav-item\">\r\n                <span class=\"nav-link\" id=\"bezos\">Jeff Bezos</span>\r\n            </li>\r\n            <li class=\"nav-item\">\r\n                <span class=\"nav-link\" id=\"alexa\">Alexa</span>\r\n            </li>\r\n            <li class=\"nav-item\">\r\n                <span class=\"nav-link\" id=\"prime\">Prime Video</span>\r\n            </li>\r\n            <li class=\"nav-item\">\r\n                <span class=\"nav-link\" id=\"grandtour\">Grand Tour</span>\r\n            </li>\r\n        </ul>\r\n        <form class=\"form-inline my-2 my-lg-0\">\r\n            <input id=\"querysearch\" class=\"form-control mr-sm-2\" navbartype=\"search\" placeholder=\"Search\" aria-label=\"Search\">\r\n            <button id=\"searchbtn\" class=\"btn btn-outline-secondary my-2 my-sm-0\" type=\"submit\">Search</button>\r\n        </form>\r\n        </div>\r\n    </nav>\r\n    `;\r\n    document.getElementById(\"navbar\").innerHTML = navbarContent;\r\n    document.getElementById(\"logo\").onclick = (event) => {\r\n        showArticles();\r\n    }\r\n    document.getElementById(\"searchbtn\").onclick = (event) => {\r\n        let query = document.getElementById(\"querysearch\").value;\r\n        showArticles(query);\r\n    }\r\n    document.getElementById(\"bezos\").onclick = (event) => {\r\n        showArticles(\"Jeff Bezos\");\r\n    }\r\n    document.getElementById(\"alexa\").onclick = (event) => {\r\n        showArticles(\"Alexa\");\r\n    }\r\n    document.getElementById(\"prime\").onclick = (event) => {\r\n        showArticles(\"Prime Video\");\r\n    }\r\n    document.getElementById(\"grandtour\").onclick = (event) => {\r\n        showArticles(\"Grand Tour\");\r\n    }\r\n\r\n};\r\n\r\nfunction showArticles(queryPhrase) {\r\n\r\n    const removeReadMoreFromDescription = (description) => {\r\n        const regex = /(.*)(Read more\\.\\.\\.)/;\r\n        let result = description.match(regex);\r\n        if (result){\r\n            return result[1];\r\n        }\r\n        return description;\r\n    };\r\n    const removeCharsFromContent = (content) => {\r\n        const regex = /(.*)(\\[\\+\\d+ chars\\])$/s;\r\n        let result = content.match(regex);\r\n        if(result){\r\n            return result[1];\r\n        }\r\n        return content;\r\n    };\r\n\r\n    const articleCard = ({urlToImage, title, description, content, url}) => `\r\n    <div class=\"card mb-3\">\r\n        <img src=\"${urlToImage}\" class=\"card-img-top\" alt=\"...\">\r\n        <div class=\"card-body\">\r\n            <h4 class=\"card-title font-weight-bolder\">${title}</h4>\r\n            <h5 class=\"card-subtitle mb-2\">${description ? removeReadMoreFromDescription(description) : description}</h5>\r\n            <p class=\"card-text\">${content ? removeCharsFromContent(content) : content}</p>\r\n            <button type=\"button\" class=\"btn btn-secondary\" onclick=\"window.open('${url}')\">Read more</button>\r\n        </div>\r\n    </div>\r\n    `;\r\n\r\n    const getFilteredArticles = (query) => {\r\n        QuerySearch.getArticles(query)\r\n        .catch((err) => {\r\n            document.getElementById(\"main\").innerHTML = `\r\n            <h1 style=\"color: red\">${err}</h1>\r\n            <h2>${err.message}</h2>\r\n            `;\r\n            throw err;\r\n        })\r\n        .then((response) => {\r\n            const articlesContainer = `\r\n            <div class=\"page-content\">\r\n                <div id=\"articles\">\r\n                    ${(response).map(data => (articleCard(data))).join(\"\")}\r\n                </div>\r\n                <div id=\"top-articles\">\r\n                    <h3>most popular articles</h3>\r\n                </div>\r\n            </div>\r\n            `;\r\n            document.getElementById(\"main\").innerHTML = articlesContainer;\r\n        });\r\n    }\r\n\r\n    const getArticles = () => Api.getArticles()\r\n    .catch((err) => {\r\n        document.getElementById(\"main\").innerHTML = `\r\n        <h1 style=\"color: red\">${err}</h1>\r\n        <h2>${err.message}</h2>\r\n        `;\r\n        throw err;\r\n    })\r\n    .then((response) => {\r\n        const articlesContainer = `\r\n        <div class=\"page-content\">\r\n            <div id=\"articles\">\r\n                ${(response).map(data => (articleCard(data))).join(\"\")}\r\n            </div>\r\n            <div id=\"top-articles\">\r\n                <h3>most popular articles</h3>\r\n            </div>\r\n        </div>\r\n        `;\r\n        document.getElementById(\"main\").innerHTML = articlesContainer;\r\n    });\r\n\r\n    if (queryPhrase) getFilteredArticles(queryPhrase);\r\n    else getArticles();\r\n    \r\n}\r\nnavbar();\r\nshowArticles();\n\n//# sourceURL=webpack:///./src/index.js?");
+
+/***/ }),
+
+/***/ "./src/style.scss":
+/*!************************!*\
+  !*** ./src/style.scss ***!
+  \************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+eval("// extracted by mini-css-extract-plugin\n\n//# sourceURL=webpack:///./src/style.scss?");
+
+/***/ })
+
+/******/ });
